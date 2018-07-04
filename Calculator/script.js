df8762870f8cdded2c8538b08bb6eff7d00380ee
bmarty92 <martynas.businskas@gmail.com>
2018-07-04 18:58:43 +0300

    added calculator

---
+++ Calculator/script.js
@@ -0,0 +1,73 @@
+// document.getElementById("sum").addEventListener("click", ()=> {
+//     let number1 = Number(document.getElementById("number1").value);
+//     let number2 = Number(document.getElementById("number2").value);
+//     ans = number1 + number2;
+//     document.getElementById("answer").innerHTML = ans;
+// });
+// document.getElementById("subtract").addEventListener("click", ()=>{
+//     let number1 = Number(document.getElementById("number1").value);
+//     let number2 = Number(document.getElementById("number2").value);
+//     ans = number1 - number2;
+//     document.getElementById("answer").innerHTML = ans;
+// });
+//
+// document.getElementById("divide").addEventListener("click", ()=>{
+//     let number1 = Number(document.getElementById("number1").value);
+//     let number2 = Number(document.getElementById("number2").value);
+//     ans = number1 / number2;
+//     document.getElementById("answer").innerHTML = ans;
+// });
+//
+// document.getElementById("multiply").addEventListener("click", ()=>{
+//     let number1 = Number(document.getElementById("number1").value);
+//     let number2 = Number(document.getElementById("number2").value);
+//     ans = number1 * number2;
+//     document.getElementById("answer").innerHTML = ans;
+// });
+//
+// document.getElementById("modulus").addEventListener("click", ()=>{
+//     let number1 = Number(document.getElementById("number1").value);
+//     let number2 = Number(document.getElementById("number2").value);
+//     ans = number1 % number2;
+//     document.getElementById("answer").innerHTML = ans;
+// });
+
+
+function calc(action){
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+    let ans = null;
+    switch (action) {
+        case 'sum':
+            ans = number1 + number2;
+            break;
+        case 'subtract':
+            ans = number1 - number2;
+            break;
+        case 'divide':
+            ans = number1 / number2;
+            break;
+        case 'multiply':
+            ans = number1 * number2;
+            break;
+        case 'modulus':
+            ans = number1 % number2;
+            break;
+        case 'clear':
+            number1 = null;
+            number2 = null;
+            break;
+    }
+    document.getElementById("answer").innerHTML = ans;
+}
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", e => {
+        calc(e.target.id);
+    })
+});
+
+
+
+
+
